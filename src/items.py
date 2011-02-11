2af71aeb6c31bc7e5b904dee940a7d628f54a20c
earthdok <drw@technocore.ru>
2011-02-11 04:27:04 +0300

    More refactorings.

---
+++ src/items.py
@@ -0,0 +1,39 @@
+from collections import namedtuple
+
+class OpenParen(object):
+    def __str__(self):
+        return '('
+open_paren = OpenParen()    
+
+class CloseParen(object):
+    def __str__(self):
+        return ')'
+close_paren = CloseParen()
+
+class Base(str):
+    def __str__(self):
+        return str.__str__(self)
+Base.I = Base('I')
+Base.C = Base('C')
+Base.F = Base('F')
+Base.P = Base('P')
+Base.decode = {'C': Base.I, 'F': Base.C, 'P': Base.F, 'IC': Base.P}
+
+class Skip(int):
+    def __str__(self):
+        return '!'+int.__str__(self)
+
+class Search(str):
+    def __str__(self):
+        return '?"'+self+'"'
+
+Reference = namedtuple('Reference', 'n level')
+class Reference(Reference):
+    def __str__(self):
+        if self.level == 0:
+            return '\\{}'.format(self.n)
+        return '\\{}_{}'.format(self.n, self.level)
+    
+class Length(int):
+    def __str__(self):
+        return '|{}|'.format(int.__str__(self)) 
