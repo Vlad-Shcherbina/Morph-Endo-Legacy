d0d6532aac8f61c09c7b79dd0e8813775dd90030
earthdok <drw@technocore.ru>
2011-02-13 01:00:47 +0300

    minor tracer tweaks incl. python 2.6 compatibility

--- src/helpers.py
+++ src/helpers.py
@@ -3,6 +3,6 @@
 def limit_string(s, maxlen=10):
     if len(s) <= maxlen:
         return ''.join(s)
-    return '{}... ({} bases)'.format(''.join(s[:maxlen]), len(s))
+    return '{0}... ({1} bases)'.format(''.join(s[:maxlen]), len(s))
 
    
