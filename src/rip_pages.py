e73479d98e5c5dbe1ec9298f734641e3c7c450eb
earthdok <drw@technocore.ru>
2011-02-16 04:01:31 +0300

    ripped a few more pages

--- src/rip_pages.py
+++ src/rip_pages.py
@@ -7,17 +7,24 @@ import dna_code as dna
 #unknown_impdocs = ["impdoc" + str(n) for n in xrange(1, 10 + 1)]
 #unknown_help = ['help_adaptive_genes', 'help_initial_cond', 
 #                'help_intro', 'help_virus', 'help_vmu']
+#unknown_fuundocs =  ["fuundoc" + str(n) for n in xrange(1, 3 + 1)]
 
 unknown_impdocs = ['impdoc3', 'impdoc4']
 unknown_help = ['help_intro']
-unknown_fuundocs =  ["fuundoc" + str(n) for n in xrange(1, 3 + 1)]
+unknown_fuundocs =  ["fuundoc1"]
 
 encrypted_help = ['help_activating_genes', 'help_error_correcting_codes', \
                   'help_beautiful_numbers']
 
-host_gene = genes.help_fuun_security
+# fuun_security as host
+#host_gene = genes.help_fuun_security
+#host_activation_prefix = \
+#    "IIPIFFCPICFPPICIICCCCCCCCCCCCCCCCCCCCCCCCIICIPPPFCFCCCFCCFCFFFCCFFCCCCFIIC"
+
+# most_wanted as host
+host_gene = genes.most_wanted
 host_activation_prefix = \
-    "IIPIFFCPICFPPICIICCCCCCCCCCCCCCCCCCCCCCCCIICIPPPFCFCCCFCCFCFFFCCFFCCCCFIIC"
+    "IIPIFFCPICFPPICIICCCCCCCCIICIPPPCCCCFFFIIC"
 
 def process_genes(names_list, prefix):
     for gene_name in names_list:
@@ -30,10 +37,10 @@ def process_genes(names_list, prefix):
                             "data\\ripped_pages\\" + gene_name)
 
 if __name__ == "__main__":
-#    prefix = dna.replace_procedure_prefix(genes.help_background, \
-#                                          genes.impdoc_background)
-#    process_genes(unknown_impdocs, prefix)
+    prefix = dna.replace_procedure_prefix(genes.help_background, \
+                                          genes.impdoc_background)
+    process_genes(unknown_impdocs, prefix)
     
-#    process_genes(unknown_help, "")
+    process_genes(unknown_help, "")
     process_genes(unknown_fuundocs, "")
     
